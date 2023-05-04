af36d8149fb9126e8ad6e1bc8f4703a2a664b23f
Elena Vasileva <123897353+Elena763@users.noreply.github.com>
2023-05-04 09:57:00 +0300

    Added files

---
+++ week9/script.js
@@ -0,0 +1,53 @@
+/*1 task*/
+let cleaner=40000;
+let teacher=22000;
+let message1;
+message1=(cleaner+teacher);
+console.log(message1);
+
+/*2 task*/
+let city='Владивосток';
+let year='2000';
+let message2;
+message2=(city+' '+year);
+console.log(message2);
+
+/*3 task*/
+let messanger = 128;
+let likes = 254;
+let vk = 137;
+let yt = 201;
+let message3;
+message3=((messanger+likes+vk+yt)/60);
+console.log(message3);
+
+/*4 task*/
+let x=2;
+let y=x**5;
+console.log(y);
+
+/*5 task*/
+let message5="Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"
+console.log(message5);
+
+/*6 task*/
+const greetings = "Привет";
+const name = "Кот";
+let message6 = greetings+', '+ name;
+console.log(message6);
+
+/*7 task*/
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time = 34-15;
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time);
+time = 19-10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log("Вы приехали. Добро пожаловать в Москву.")
+
+/*8 task*/
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(fahrenheit + 'градуса по Фаренгейту - это' + celsius + 'градуса по Цельсию.');
